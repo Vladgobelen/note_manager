24618e1a3f1f82bfa8985adaa06639023ba02b03
Vladgobelen <vladgobelen@gmail.com>
2025-01-03 23:09:21 +0700

    релиз

--- greetings.py
+++ greetings.py
@@ -1,13 +1,15 @@
-#"1st program"
-print(9 ** 0.5 * 5)
+# variables
+username = 'Владгобелен'
+title = 'Очень важная заметка'
+content = 'Глубокомысленные размышления о том, что нужно сделать'
+status = 'В работе'
+created_date = '03-01-2025'
+issue_date = '04-01-2025'
 
-#"2nd program"
-print(9.99 > 9.98 and 1000 != 1000.1)
-
-#"3rd program"
-print(2 * 2 + 2)
-print(2 * (2 + 2))
-print((2 * 2 + 2) == (2 * (2 + 2)))
-
-#"4th program"
-print((int(float('123.456') * 10)) % 10)
+# working
+print('Имя пользователя:', username)
+print('Заголовок заметки:', title)
+print('Описание заметки:', content)
+print('Статус заметки:', status)
+print('Дата создания заметки:', created_date)
+print('Дата истечения заметки:', issue_date)
