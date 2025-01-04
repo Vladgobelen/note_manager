a3043aab3216a283276e3c604a82b4061c20f63a
Vladgobelen <vladgobelen@gmail.com>
2025-01-04 19:32:42 +0700

    quest2

---
+++ date_changer.py
@@ -0,0 +1,15 @@
+# variables
+username = 'Владгобелен'
+title = 'Очень важная заметка'
+content = 'Глубокомысленные размышления о том, что нужно сделать'
+status = 'В работе'
+created_date = '03-01-2025'
+issue_date = '04-01-2025'
+
+# working
+print('Имя пользователя:', username)
+print('Заголовок заметки:', title)
+print('Описание заметки:', content)
+print('Статус заметки:', status)
+print('Дата создания заметки:', "-".join(created_date.split(sep="-")[:2]))
+print('Дата истечения заметки:', "-".join(issue_date.split(sep="-")[:2]))
