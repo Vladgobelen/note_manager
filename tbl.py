216f04bd5f394687a36325913134faf9c830a2b4
Vladgobelen <vladgobelen@gmail.com>
2025-01-06 03:49:53 +0700

    add for

---
+++ tbl.py
@@ -0,0 +1,36 @@
+# variables
+note = {
+        'username': {
+                    "var": [],
+                    "comment": ['Имя пользователя']
+                    },
+        'title1': {
+                    "var": [],
+                    "comment": [
+                                "Заголовок заметки 1",
+                                "Заголовок заметки 2",
+                                "Заголовок заметки 3"
+                                ]},
+        'content': {
+                    "var": [],
+                    "comment": ["Описание заметки"]},
+        'status': {
+                    "var": [],
+                    "comment": ["Статус заметки"]},
+        'created_date': {
+                        "var": [],
+                        "comment": ["Дата создания заметки(ДД-ММ-ГГГГ)"]},
+        'issue_date': {
+                        "var": [],
+                        "comment": ["Дата истечения заметки(ДД-ММ-ГГГГ)"]}
+        }
+# working
+print("Введите следущие данные: \n")
+for key, value in note.items():
+    for i in range(0, len(note[key]["comment"])):
+        note[key]['var'].append(input(f'{note[key]["comment"][i]}: '))
+
+print("\n")
+for key, value in note.items():
+    for i in range(0, len(note[key]["comment"])):
+        print(f'{note[key]["comment"][i]}: ', note[key]["var"][i])
