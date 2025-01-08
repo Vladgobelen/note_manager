00514147389418f24e02a43f058b222a022d3292
Vladgobelen <vladgobelen@gmail.com>
2025-01-08 21:15:48 +0700

    add loop2

---
+++ add_titles_loop.py
@@ -0,0 +1,45 @@
+# vars
+note = {
+        'username': {
+                    "var": [],
+                    "comment": ['Имя пользователя']
+                    },
+        'title1': {
+                    "var": [],
+                    "comment": ["Заголовок заметки"]
+                    },
+        'content': {
+                    "var": [],
+                    "comment": ["Описание заметки"]
+                    },
+        'status': {
+                    "var": [],
+                    "comment": ["Статус заметки"]
+                    },
+        'created_date': {
+                        "var": [],
+                        "comment": ["Дата создания заметки(ДД-ММ-ГГГГ)"]
+                        },
+        'issue_date': {
+                        "var": [],
+                        "comment": ["Дата истечения заметки(ДД-ММ-ГГГГ)"]
+                        }
+        }
+# working
+print("Введите следущие данные(для завершения введите пустую строку): \n")
+for key in note:
+    while True:
+        temp_text = input(f'{note[key]['comment'][0]}: ')
+        if temp_text == "":
+            break
+        else:
+            note[key]['var'].append(temp_text)
+
+print("\n")
+for key in note:
+    for var in note[key]["var"]:
+        if key == "created_date" or key == "issue_date":
+            value = "-".join(var.split(sep="-")[:2])
+            print(f'{note[key]["comment"][0]}: ', value)
+        else:
+            print(f'{note[key]["comment"][0]}: ', var)
