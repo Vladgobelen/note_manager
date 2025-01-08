c9c3de3bab8bc51af024f1ae548afa193fb7bc48
Vladgobelen <vladgobelen@gmail.com>
2025-01-09 05:26:21 +0700

    status update

--- update_status.py
+++ update_status.py
@@ -9,7 +9,7 @@ note = {
 # defs
 
 
-def get_var(key, link):
+def get_var(key, link):  # Universal obtaining of variable values
     if link:
         for i, values in enumerate(note[key]['var']):
             print(f'{note[key]['comment'][i]}: {values}')
@@ -18,7 +18,7 @@ def get_var(key, link):
             print(f'{note[key]['comment'][0]}: {values}')
 
 
-def set_key(key, value, mod, message):
+def set_key(key, value, mod, message):  # Variable entry with validation
     if note[key]['check']:
         if check_key(key, message):
             note[key][value][mod] = message
@@ -28,20 +28,19 @@ def set_key(key, value, mod, message):
             print(*note[key]['check'], sep="\n")
 
 
-def check_key(key, words):
+def check_key(key, words):  # Checking for a word match with the dictionary
     for word in note[key]['check']:
         if word in words:
             return 1
 
 
 # works
-get_var("status", None)
-index = 0
-while True:
+get_var("status", None)  # Getting the current status.
+while True:  # Recording a new status
     status = note['status']['comment'][0]
     var = input(f'Укажите новый {status} или введите ничего для завершения: ')
-    if var == "":
+    if var == "":  # Exiting the program
         get_var("status", None)
         break
-    else:
+    else:  # Status changed
         set_key('status', 'var', 0, var)
