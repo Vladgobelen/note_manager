47a36c3e41872e6cd5b8f474d1f45c6143d70839
Vladgobelen <vladgobelen@gmail.com>
2025-01-10 18:09:26 +0700

    deadline add

--- check_deadline1.py
+++ check_deadline1.py
@@ -1,5 +1,6 @@
 import datetime
 from datetime import datetime as dt
+from copy import deepcopy
 # vars
 notes = []
 note = {
@@ -41,26 +42,26 @@ note = {
 # defs
 
 
-def get_var(key, link, end, n_index):  # Universal obtaining of variable values
+def get_var(key, link, end, note):  # Universal obtaining of variable values
     end_text = ""
     if end:
         end_text = "сохранено как"
     else:
         end_text = "изменено на"
     if link:
-        for i, values in enumerate(notes[n_index][key]['var']):
+        for i, values in enumerate(note[key]['var']):
             if key == "issue_date" or key == "created_date":
                 format_date = "-".join(values.split(sep="-")[:2])
-                print(f'{notes[n_index][key]['comment'][i]} {n_index} {end_text}: {format_date}')
+                print(f'{note[key]['comment'][i]} {end_text}: {format_date}')
             else:
-                print(f'{notes[n_index][key]['comment'][i]} {n_index} {end_text}: {values}')
+                print(f'{note[key]['comment'][i]} {end_text}: {values}')
     else:
-        for i, values in enumerate(notes[n_index][key]['var']):
+        for i, values in enumerate(note[key]['var']):
             if key == "issue_date" or key == "created_date":
                 format_date = "-".join(values.split(sep="-")[:2])
-                print(f'{notes[n_index][key]['comment'][0]} {n_index} {end_text}: {format_date}')
+                print(f'{note[key]['comment'][0]} {end_text}: {format_date}')
             else:
-                print(f'{notes[n_index][key]['comment'][0]} {n_index} {end_text}: {values}')
+                print(f'{note[key]['comment'][0]} {end_text}: {values}')
 
 
 def set_key(key, value, mod, message, n_index):  # Variable entry
@@ -68,9 +69,9 @@ def set_key(key, value, mod, message, n_index):  # Variable entry
         if check_key(key, message) or notes[n_index][key]['check'][0] == "all":
             notes[n_index][key]['var'].append(message)
             if key == 'content':
-                get_var(key, 1, None, n_index)
+                get_var(key, 1, None, notes[n_index])
             else:
-                get_var(key, None, None, n_index)
+                get_var(key, None, None, notes[n_index])
         else:
             print(f'"{message}" является недопустимым словом. Выберите из: ')
             print(*notes[n_index][key]['check'], sep="\n")
@@ -82,28 +83,37 @@ def check_key(key, words):  # Checking for a word match with the dictionary
             return 1
 
 
+def check_key(key, words):  # Checking for a word match with the dictionary
+    for word in note[key]['check']:
+        if word in words:
+            return 1
+
+
 def get_current_day(my_date):
-    return my_date.strftime('%d')
+    return my_date.days
 
 
-def chek_date(first_date, second_date):
-    return int(get_current_day(first_date))-int(get_current_day(second_date))
+def check_date(first_date, second_date):
+    return get_current_day((first_date)-(second_date))
 
 
-def get_date(issue_date):
+def get_date(my_date):
     today = datetime.datetime.now()
-    check_day = dt.strptime(issue_date, '%d-%m-%Y')
-    if chek_date(today, check_day) > 0:
-        print(f"Дедлайн прошел {chek_date(today, check_day)} дней назад")
+    check_day = dt.strptime(my_date, '%d-%m-%Y')
+    if check_date(today, check_day) > 0:
+        print(f"Дедлайн прошел {check_date(today, check_day)} дней назад")
+        return 1
     else:
-        print(f"Дедлайн через {-1*int(chek_date(today, check_day))} дней")
+        print(f"Дедлайн через {-1*int(check_date(today, check_day))} дней")
+        return 1
 
 
 # works
 print("Введите следущие данные(для завершения введите пустую строку): \n")
 n_index = 0
-while True:
-    notes.append(note)
+need_break = "да"
+while need_break == "да":
+    notes.append(deepcopy(note))
     for key in note:
         index = 0
         while True:
@@ -111,8 +121,8 @@ while True:
             if temp_text == "":
                 notes[n_index][key]['var'] = list(set(notes[n_index][key]['var']))
                 notes[n_index][key]['var'].sort()
-                print('112', notes)
                 if key == "issue_date":
+                    need_break = input("Добавить еще одну заметку?(да нет)")
                     n_index += 1
                 break
             else:
@@ -120,15 +130,13 @@ while True:
                 index += 1
 
 print("\n")
-n_index = 0
-for i in range(0, len(notes)):
-    for kye in notes[i]:
-        n_index = 0
+for note in notes:
+    for kye in note:
         if key == 'content':
-            get_var(key, 1, 1, n_index)
+            get_var(key, 1, 1, note)
         else:
             if key == "issue_date":
-                get_var(key, None, 1, n_index)
-                get_date(notes[n_index][key]['var'][0])
+                get_var(key, None, 1, note)
+                get_date(note[key]['var'][0])
             else:
-                get_var(key, None, 1, n_index)
+                get_var(key, None, 1, note)
