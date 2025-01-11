591f7a004e2ec0c463ad541fe50ec45fc27ae6dc
Vladgobelen <vladgobelen@gmail.com>
2025-01-11 17:04:57 +0700

    final add

--- test.py
+++ test.py
@@ -5,39 +5,39 @@ from copy import deepcopy
 notes = []
 note = {
         'delete': {
-                    "var": "",
+                    "var": [],
                     "comment": "Выберите: удалить, создать заметку или выйти",
                     "check": ["удалить", "создать", "выйти"],
                     "check_del": ["все заметки", "по заголовку", "по имени"]
                     },
         'username': {
-                    "var": ["Влад"],
-                    "comment": 'Имя пользователя',
+                    "var": [],
+                    "comment": 'имя пользователя',
                     "check": ["all"]
                     },
         'title': {
-                    "var": ["первый", "второй"],
-                    "comment": ["Заголовок заметки"],
+                    "var": [],
+                    "comment": "заголовок заметки",
                     "check": ["all"]
                     },
         'content': {
-                    "var": "Тестовая заметка",
-                    "comment": "Описание заметки",
+                    "var": [],
+                    "comment": "описание заметки",
                     "check": ["all"]
                     },
         'status': {
-                    "var": "",
-                    "comment": ["Статус заметки"],
+                    "var": [],
+                    "comment": "статус заметки",
                     "check": ["в процессе", "выполнено", "отложено"]
                     },
         'created_date': {
-                        "var": "",
-                        "comment": ["Дата создания заметки(ДД-ММ-ГГГГ)"],
+                        "var": [],
+                        "comment": "Дата создания заметки(ДД-ММ-ГГГГ)",
                         "check": ["all"]
                         },
         'issue_date': {
-                        "var": "",
-                        "comment": ["Дата истечения заметки(ДД-ММ-ГГГГ)"],
+                        "var": [],
+                        "comment": "Дата истечения заметки(ДД-ММ-ГГГГ)",
                         "check": ["all"]
                         }
         }
@@ -70,13 +70,13 @@ def del_by(key, del_word):  # deleting a note by parameter
                     notes.append(deepcopy(note))
 
 
-def check_exit():  # checking the correct input(exit)
-    word = input(comment(note, "delete") + ": ")
-    if check_key(notes[len(notes)-1], "delete", "check", word):
+def check_exit(key):  # checking the correct input(exit)
+    word = input(comment(note, key) + ": ")
+    if check_key(notes[len(notes)-1], key, "check", word):
         return word
     else:
         print(f'"{word}" является недопустимым словом. Выберите из: \n')
-        print(*notes[len(notes)-1]["delete"]['check'], sep="\n")
+        print(*notes[len(notes)-1][key]['check'], sep="\n")
         return ""
 
 
@@ -106,11 +106,12 @@ def print_notes():  # summary of the notes
         result = "\nЗаметки: \n"
         result = result + "\nИмя: " + note['username']['var'][0] + "\n"
         result = result + "Заголовки: "
-        i = 1
+        i = 0
         for titles in note['title']['var']:
-            result = result + str(i) + ") " + titles + " "
+            result = result + str(i+1) + ") " + titles + " "
+            i += 1
         result = result + "\nОписание: "
-        result = result + note['content']['var'] + "\n"
+        result = result + note['content']['var'][0] + "\n"
         return result
 
 
@@ -122,16 +123,67 @@ def show_print():  # information about notes
         notes.append(deepcopy(note))
 
 
+def input_title():
+    title = []
+    while True:
+        print("Введите заголовок или нажмите энтер для завершения: ", end="")
+        word = input()
+        if word == "":
+            break
+        else:
+            title.append(word)
+    return title
+
+
+def input_status():
+    print("\nВведите статус заметки: ", end="")
+    print(*note["status"]['check'], sep=", ")
+    while True:
+        word = check_exit("status")
+        if word != "":
+            break
+    return word
+
+
+def input_date(key):
+    format = "%d-%m-%Y"
+    while True:
+        created_date = input(comment(note, key) + ': ')
+        try:
+            bool(dt.strptime(created_date, format))
+            return created_date
+        except ValueError:
+            print("Неправильный формат даты (ДД-ММ-ГГГ), попробуйте еще раз: ")
+
+
+def create_note():
+    while True:
+        name = input('Введите ' + comment(note, "username") + ': ')
+        notes[len(notes)-1]['username']['var'].append(name)
+        title = input_title()
+        notes[len(notes)-1]['title']['var'] = title
+        content = input('Введите ' + comment(note, "content") + ': ')
+        notes[len(notes)-1]['content']['var'].append(content)
+        status = input_status()
+        notes[len(notes)-1]['status']['var'].append(status)
+        create = input_date('created_date')
+        notes[len(notes)-1]['created_date']['var'].append(create)
+        create = input_date('issue_date')
+        notes[len(notes)-1]['issue_date']['var'].append(create)
+        print(notes)
+        break
+
+
 def work(need_break):  # The main code
     show_print()
     while need_break == "да":
-        exit_word = check_exit()
-        if (exit_word) == "выйти":
+        exit_word = check_exit("delete")
+        if exit_word == "выйти":
             break
-        elif (exit_word) == "удалить":
+        elif exit_word == "удалить":
             del_note()
-        else:
-            pass
+        elif exit_word == "создать":
+            create_note()
         show_print()
 
 
